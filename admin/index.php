8f080ec9c3f05712b84557ae4bbdf2046fba157f
Saw Theodore <sawtheodore@gmail.com>
2024-11-20 11:25:13 +0630

    side

--- admin/index.php
+++ admin/index.php
@@ -27,6 +27,27 @@
             </div>
         </div>
        </div>
+       <div class="sidebar-menu"  data-bs-toggle="collapse" data-bs-target="#user-menu">
+            <div>
+                <i class="fa fa-users"></i>
+                <a  style="width: 100%;"> Category </a> 
+            </div> 
+            <i class="fa fa-angle-down"></i>
+       </div>
+       <div class="sidebar-sub-menu collapse" id="user-menu">
+        <div class="sidebar-menu">
+               <div>
+                    <i class="fa fa-user-plus"></i>
+                    <a href=""> Add Category </a>
+               </div>
+        </div>
+        <div class="sidebar-menu">
+            <div>
+                <i class="fa fa-list"></i>
+                <a> Category List </a>
+            </div>
+        </div>
+       </div>
        <div class="divition"></div>
        <div class="toggle">
             <i id="sidebar-toggle" class="fa fa-angle-left"></i>
