12c41b45e8a91e6d250a812c102b9a95c989c754
Saw Theodore <sawtheodore@gmail.com>
2024-11-20 10:14:26 +0630

    toggle

--- admin/index.php
+++ admin/index.php
@@ -1,10 +1,14 @@
 <?php require_once ("../layout/header.php") ?>
 <div class="main d-flex">
-    <div class="sidebar extend">
+    <div id="sidebar" class="sidebar extend">
         <div class="sidebar-header">
             <i class="fa fa-smile"></i>
             <h5 class="sidebar-label">JO JO Hotpot</h5>
         </div>
+       <div class="divition"></div>
+       <div class="toggle">
+            <i id="sidebar-toggle" class="fa fa-angle-left"></i>
+       </div>
     </div>
     <div class="content">
 b
--- layout/footer.php
+++ layout/footer.php
@@ -1,2 +1,10 @@
+<script>
+    let toggle = $("#sidebar-toggle");
+    let sidebar = $("#sidebar");
+    toggle.on("click",()=>{
+        toggle.toggleClass("fa-angle-right");
+        sidebar.toggleClass("extend");
+    })
+</script>
 </body>
 </html>
